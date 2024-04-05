3a544bfe7f252dee6f6030ca3c2e09ceb04cd2a6
lilliputten <lilliputten@gmail.com>
2024-04-05 17:54:02 +0700

    Issue #40: Updated version: 0.0.15.

--- dds_registration/__init__.py
+++ dds_registration/__init__.py
@@ -8,8 +8,8 @@ stripe.api_key = settings.STRIPE_SECRET_KEY  # 'sk_...'
 
 # Project version properties
 __version__ = "0.0.15"
-__timestamp__ = "2024.04.05 17:43 +0700"
-__timetag__ = "240405-1743"
+__timestamp__ = "2024.04.05 17:53 +0700"
+__timetag__ = "240405-1753"
 
 
 __all__ = (
